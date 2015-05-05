cd686ce1989f77d45bc0ae43179e9622395d9f1d
YoungHuang <sean0722@msn.com>
2015-05-05 18:07:34 +0800

    添加参数设置。

--- src/GameMenuScene.js
+++ src/GameMenuScene.js
@@ -8,21 +8,26 @@ var GameMenuLayer = cc.Layer.extend({
         this._super();
 
         //获取设备尺寸
-        var winSize = cc.winSize;
+        //var winSize = cc.winSize;
 
-        //添加背景
-        var GMBackground = new cc.Sprite(res.GameMenuBG_png);
-        GMBackground.attr({
-            x : winSize.width / 2, //定位
-            y : winSize.height / 2,
-            scale: 0.5 //中心点
-        });
-        this.addChild(GMBackground, 0);
+        this.GMBackground(); //背景
 
         //添加背景音乐
         //cc.audioEngine.playMusic(res.GameMenuBG_mp3, true);
 
         return true;
+    },
+
+    //添加背景
+    GMBackground:function(){
+        //添加背景
+        var GMBackground = new cc.Sprite(res.GameMenuBG_png);
+        GMBackground.attr({
+            x : GC.winSize / 2, //定位
+            y : GC.winSize / 2,
+            scale: 0.5 //中心点
+        });
+        this.addChild(GMBackground, 0);
     }
 });
 
